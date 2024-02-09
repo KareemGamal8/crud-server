bcd0b9d56f930a310ec49012645bf54eb63560c3
KareemGamal07 <kareemgmal7755@gmail.com>
2024-02-09 22:08:25 +0200

    Updates

---
+++ index.js
@@ -0,0 +1,24 @@
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+const cors = require("cors");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+// Enable CORS for all routes
+server.use(cors());
+
+// Add authentication middleware
+server.db = router.db;
+server.use(auth);
+
+// Set default middlewares (logger, static, cors, etc.)
+server.use(middlewares);
+
+// Use default router
+server.use(router);
+
+// Start server
+const port = 3000;
+
+server.listen(port);
